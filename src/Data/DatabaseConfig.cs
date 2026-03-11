bbebe75b43ef5defab6fe1442f5d3cead8200b80
nirmalat <tamang07nirmala@gmail.com>
2026-03-11 17:00:28 -0400

    Aded database configuration using appsettings.json

---
+++ src/Data/DatabaseConfig.cs
@@ -0,0 +1,25 @@
+using MySql.Data.MySqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace BankingTransactionSystem.Data
+{
+    public class DatabaseConfig
+    {
+        private readonly string _connectionString;
+
+        public DatabaseConfig()
+        {
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+         
+        }
+
+        public MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(_connectionString);
+        }
+    }
+}
