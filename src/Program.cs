9d847c3a3f3ad26e1d9081e884f8c2f32951c6fb
nirmalat <tamang07nirmala@gmail.com>
2026-03-11 20:12:21 -0400

    Added menu handler with role-based login flow

--- src/Program.cs
+++ src/Program.cs
@@ -1,5 +1,4 @@
-﻿using BankingTransactionSystem.Services;
-using BankingTransactionSystem.Models;
+﻿using BankingTransactionSystem.UI;
 
 namespace BankingTransactionSystem
 {
@@ -7,28 +6,8 @@ namespace BankingTransactionSystem
     {
         public static void Main(string[] args)
         {
-            Console.Write("Enter login: ");
-            string login = Console.ReadLine() ?? "";
-
-            Console.Write("Enter PIN: ");
-            int pin = int.Parse(Console.ReadLine() ?? "0");
-
-            AuthService authService = new AuthService();
-
-            Account? account = authService.Login(login, pin);
-
-            if (account != null)
-            {
-                Console.WriteLine("\nLogin successful!");
-                Console.WriteLine($"Account #: {account.AccountNumber}");
-                Console.WriteLine($"Holder: {account.HolderName}");
-                Console.WriteLine($"Balance: {account.Balance}");
-                Console.WriteLine($"Role: {account.Role}");
-            }
-            else
-            {
-                Console.WriteLine("\nInvalid login or PIN.");
-            }
+            MenuHandler menuHandler = new MenuHandler();
+            menuHandler.Start();
         }
     }
 }
